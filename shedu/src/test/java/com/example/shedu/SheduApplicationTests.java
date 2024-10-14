4375a727e3ad6ccd709d77c2b5053d57bf474d60
DevCod18 <mnabiyev077@gmail.com>
2024-10-14 14:25:49 +0500

    shedu
    New Project SHEDU

---
+++ shedu/src/test/java/com/example/shedu/SheduApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shedu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SheduApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
