2fe65ac446351cff2c6cbdcb832feb736b7d602f
Muhammad <mnabiyev077@gmail.com>
2024-10-19 16:54:14 +0500

    men feedback ni optimallashtirdim

--- src/main/java/com/example/shedu/repository/FeedbackRepository.java
+++ src/main/java/com/example/shedu/repository/FeedbackRepository.java
@@ -7,9 +7,14 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FeedbackRepository extends JpaRepository<Feedback, Long> {
+    Page<Feedback> findByUserId(Long barberId, Pageable pageable);
+
+    Page<Feedback> findByBarbershopIdAndRatingLessThanEqual(Long barbershopId, int rating, Pageable pageable);
+    Page<Feedback> findByBarbershopIdAndRating(Long barbershopId, int rating, Pageable pageable);
+    Page<Feedback> findByBarbershopIdAndRatingGreaterThanEqual(Long barbershopId, int rating, Pageable pageable);
     Page<Feedback> findByBarbershopId(Long barbershopId, Pageable pageable);
 
-    Page<Feedback> findByUserId(Long barberId, Pageable pageable);
+
 }
 
 
